cbde58cb8bee2d3d13362c0fd4f24eff7265288f
Echoic88 <52455390+Echoic88@users.noreply.github.com>
2020-05-18 13:11:07 +0100

    Fix issue with  password reset email Accounts app: Urls: password_reset urls  - amend "html_email_template_name" to  "email_template_name". Also change passeord_reset_complete url to point to "accounts/registration/password_reset_complete.html" template.
    Templates:
    password_reset_email.html - amend url link in template to  correct url
    {% url "accounts:password_reset_confirm" uidb64=uid token=token %}

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path, include, reverse_lazy
 from .views import signup, signup_pending, activate, signin, signout
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 
 app_name = "accounts"
@@ -11,23 +11,23 @@ urlpatterns = [
         activate, name='activate'),
     path("signin/", signin, name="signin"),
     path("signout", signout, name="signout"),
-    path("password_reset/", views.PasswordResetView.as_view(
+    path("password_reset/", auth_views.PasswordResetView.as_view(
         template_name="accounts/registration/password_reset_form.html",
         success_url=reverse_lazy("accounts:password_reset_done"),
-        html_email_template_name="accounts/registration/password_reset_email.html"
+        email_template_name="accounts/registration/password_reset_email.html"
         ), 
         name="password_reset"),
-    path("password_reset/done/", views.PasswordResetDoneView.as_view(
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(
         template_name="accounts/registration/password_reset_done.html"
         ),
         name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", views.PasswordResetConfirmView.as_view(
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
         template_name="accounts/registration/password_reset_form.html",
         success_url=reverse_lazy("accounts:password_reset_complete")
         ),
         name="password_reset_confirm"),
-    path("reset/done/", views.PasswordResetCompleteView.as_view(
-        template_name="accounts/registration/password_complete.html"
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/registration/password_reset_complete.html"
         ),
         name="password_reset_complete"),
 ]
