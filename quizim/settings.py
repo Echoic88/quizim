9028963ec152c0dffbc74203c1b78f70006560ac
Echoic88 <52455390+Echoic88@users.noreply.github.com>
2020-05-26 14:40:02 +0100

    Update allowed hosts settings.py Update allowed hosts and comment out "import env"

--- quizim/settings.py
+++ quizim/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import env
+#import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "quizim.herokuapp.com/"]
 
 
 # Application definition
