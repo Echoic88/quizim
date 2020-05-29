4a8d3c6b859c66105510cbee54042e8fd9f44992
Echoic88 <52455390+Echoic88@users.noreply.github.com>
2020-05-29 18:51:26 +0100

    Prevent users from buying the same quiz twice Store app: views: index view - now passes list of quizes already purchased to the template templates: index.html - add logic to only display add/remove from cart buttons if the quiz has not been previously purchased

--- store/views.py
+++ store/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from quiz.models import PaidQuiz
+from quiz.models import PaidQuiz, Order
 
 # Create your views here.
 def index(request):
     """
     Display main store page
     """
-    quizes = PaidQuiz.objects.all() 
+    quizes = PaidQuiz.objects.all()
+    previous_purchases = Order.objects.filter(customer=request.user)
+
+    previous_purchases_list = []
+
+    for quiz in quizes:
+        for previous in previous_purchases:
+            if previous.quiz == quiz.quiz:
+                previous_purchases_list.append(quiz)
+
     return render(request, "store/index.html", {
-        "quizes":quizes
+        "quizes":quizes,
+        "previous_purchases_list":previous_purchases_list
     })
