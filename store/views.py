5d88f7b1ec4c0c6b64b5e634ee6d340cc621f0b8
Echoic88 <52455390+Echoic88@users.noreply.github.com>
2020-05-30 16:12:51 +0100

    Change navbar Store app: views: index view - If user not logged in redirect to home page with message
    Root:
    templates:
    base.html remove cart from navbar if user not logged

--- store/views.py
+++ store/views.py
@@ -1,22 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from quiz.models import PaidQuiz, Order
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     """
     Display main store page
     """
-    quizes = PaidQuiz.objects.all()
-    previous_purchases = Order.objects.filter(customer=request.user)
+    if request.user.is_authenticated:
+        quizes = PaidQuiz.objects.all()
+        previous_purchases = Order.objects.filter(customer=request.user)
 
-    previous_purchases_list = []
+        previous_purchases_list = []
 
-    for quiz in quizes:
-        for previous in previous_purchases:
-            if previous.quiz == quiz.quiz:
-                previous_purchases_list.append(quiz)
+        for quiz in quizes:
+            for previous in previous_purchases:
+                if previous.quiz == quiz.quiz:
+                    previous_purchases_list.append(quiz)
 
-    return render(request, "store/index.html", {
-        "quizes":quizes,
-        "previous_purchases_list":previous_purchases_list
-    })
+        return render(request, "store/index.html", {
+            "quizes":quizes,
+            "previous_purchases_list":previous_purchases_list
+        })
+
+    else:
+        messages.info(request, "Please log in to access the Store")
+        return redirect(reverse("home:index"))
