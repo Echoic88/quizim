219022f1a39e73ee277d6c7f5972c01276647cc8
Echoic88 <52455390+Echoic88@users.noreply.github.com>
2020-05-31 12:52:11 +0100

    Add first name and last name fields to Userarea Userarea app: forms: UserUpdateForm - add first name and last name fields to form

--- userarea/forms.py
+++ userarea/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ["username", "first_name", "last_name", "email"]
 
     def __init__(self, *args, **kwargs):
         super(UserUpdateForm, self).__init__(*args, **kwargs)
